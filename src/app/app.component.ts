0fdb8ee13f05b10cc2b6f27363f92386b5a8a454
VMescape <goldpatrick@hotmail.com>
2024-10-15 17:31:12 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'passwords';
+  includeUppercase = true; // Enabled by default
+  includeSymbols = true;   // Enabled by default
+  passwordLength = 8;      // Ensure the initial value is at least 8
+  generatedPassword = '';
+
+  generatePassword() {
+    const numbers = '0123456789';
+    const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+    const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+    let validChars = lowerCaseLetters;
+    if (this.includeUppercase) {
+      validChars += upperCaseLetters;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let password = '';
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      password += validChars[index];
+    }
+
+    this.generatedPassword = password;
+  }
 }
